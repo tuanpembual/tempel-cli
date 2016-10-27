a5bd9c4e835c151b576bd9690342ecb29ce28d2d
Ngalim Siregar <ngalim.siregar@gmail.com>
2016-10-27 12:50:24 +0900

    update readme, set default lang as text

--- tempel.py
+++ tempel.py
@@ -13,33 +13,39 @@ def get_args():
         epilog=None
     )
     #required args
-    parser.add_argument('-l', '--language', action='store', required=True, 
-                    help='set language of source code; bash c cpp css diff html html+django ini java lua make perl php python rst ruby sql text xml yaml')
+    parser.add_argument('language', action='store', nargs='?',
+                    help='language of source code; bash c cpp css diff html html+django ini java lua make perl php python rst ruby sql text xml yaml',
+                    default='text', choices=['bash', 'c', 'cpp', 'css', 'diff', 'html', 'html+django', 'ini', 'java', 'lua', 'make', 'perl', 'php', 'python',
+                    'rst', 'ruby', 'sql', 'text', 'xml', 'yaml'])
 
     group = parser.add_mutually_exclusive_group()
     #optional args
     group.add_argument('-t', '--text', help='from text', default=False)
-    group.add_argument('-c', '--clipboard', help='from clipboard', default=True)
     group.add_argument('-f', '--filename', help='from file', default=False)    
 
     parser.add_argument('-v', '--version', action='version',
                     version='%(prog)s {version}'.format(version=__version__))
 
     args = parser.parse_args()
-    if args.text:
-        content = args.text
-    elif args.filename:
-        content = from_file(args.filename)
+    if args.language in ['bash', 'c', 'cpp', 'css', 'diff', 'html', 'html+django',
+                         'ini', 'java', 'lua', 'make', 'perl', 'php', 'python',
+                         'rst', 'ruby', 'sql', 'text', 'xml', 'yaml'
+                        ]:
+        if args.text:
+            content = args.text
+        elif args.filename:
+            content = from_file(args.filename)
+        else:
+            content = clipboard.paste()
+
+        tempel = 'http://tempel.blankon.in'
+        values = {
+        'language' : args.language,
+        'content' : content
+        }
+        print 'URL: ', post(tempel, values)
     else:
-        content = clipboard.paste()
-    
-    tempel = 'http://tempel.blankon.in'
-    values = {
-      'language' : args.language,
-      'content' : content
-    }
-    response = requests.request("POST", tempel, data=values)
-    print 'URL: ', response.url
+        print('Please use specified language in help')
 
 def from_file(berkas):
     """"""
@@ -49,6 +55,12 @@ def from_file(berkas):
     except ValueError as e:
         print(e)
 
+def post(url, values):
+    try:
+        response = requests.request('POST', url, data=values)
+        return response.url
+    except ValueError as e:
+        print(e)
 
 if __name__ == "__main__":
    #main(sys.argv[1:])
